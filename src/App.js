bb0104d0b330cd56c0f4f0b4ed7882528fa8ba7b
Toushif <toushif.haq@gmail.com>
2021-11-17 10:09:32 +0530

    XMLHttpRequest changes

--- src/App.js
+++ src/App.js
@@ -16,28 +16,30 @@ function App() {
   const [colorData, setColorData] = useState(null)
 
   useEffect(() => {
-    async function fetchMyAPI() {
-      let response = await fetch('https://random-flat-colors.vercel.app/api/random?count=6')
-      response = await response.json()
-      setColorData(response.colors)
-    }
+    // async function fetchMyAPI() {
+    //   let response = await fetch('https://random-flat-colors.vercel.app/api/random?count=6')
+    //   response = await response.json()
+    //   setColorData(response.colors)
+    // }
 
-    fetchMyAPI()
+    // fetchMyAPI()
 
     // let res = fetchAPI()
     // res.then(data => {
     //   setColorData(data.colors)
     // })
     
-    // const xmlResponse = new XMLHttpRequest();
-    // xmlResponse.open('GET', 'https://random-flat-colors.vercel.app/api/random?count=6', true)
-    // xmlResponse.onload = function() {
-    //   if(xmlResponse.status === 200) {
-    //     const data = JSON.parse(xmlResponse.response);
-    //     setColorData(data.colors)
-    //   }
-    // };
-    // xmlResponse.send()
+    const xmlResponse = new XMLHttpRequest();
+    xmlResponse.open('GET', 'https://random-flat-colors.vercel.app/api/random?count=6', true)
+    xmlResponse.onload = function() {
+      if(xmlResponse.status === 200) {
+        const data = xmlResponse.response;
+        console.log(xmlResponse.response)
+        setColorData(data.colors)
+      }
+    };
+    xmlResponse.responseType = 'json' //if you dont use this then xmlResponse.response inside onload will be string by default whicih you will need to parse using JSON.parse(xmlResponse.response)
+    xmlResponse.send()
   }, [])
 
   return (
